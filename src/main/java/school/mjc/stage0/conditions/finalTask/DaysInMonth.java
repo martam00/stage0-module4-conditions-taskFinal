0a1355002cad31073eec73ba1f9fa651281be5d8
martam00 <marta.michniacka1@gmail.com>
2023-07-22 23:08:32 +0200

    final task completed

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -3,5 +3,21 @@ package school.mjc.stage0.conditions.finalTask;
 public class DaysInMonth {
     public void printDays(int year, int month) {
 
+        if (year < 0 || month > 12 || month < 1) {
+            System.out.println("invalid date");
+        }
+        switch (month) {
+            case 1, 3, 5, 7, 8, 10, 12 -> System.out.println("31");
+            case 4, 6, 9, 11 -> System.out.println("30");
+            case 2 -> {
+                if ((year % 4 == 0 && year % 100 != 0) || (year % 4 == 0 && year % 100 == 0 && year % 400 == 0)) {
+                    System.out.println("29");
+                } else {
+                    System.out.println("28");
+                }
+            }
+        }
+
+
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,25 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+
+        // integer division of dividend be divider, save the value
+        // multiply result by divider and will if
+        //got value is equal to dividend will print:"can be divided completely",
+        // otherwise "cannot be divided completely" or
+        //"division by zero".
+
+        if (divider == 0) {
+            System.out.println("division by zero");
+        } else {
+            int result = dividend / divider;
+            result *= divider;
+
+            if (result == dividend) {
+                System.out.println("can be divided completely");
+            } else {
+                System.out.println("cannot be divided completely");
+            }
+        }
+
     }
 }
